54c25aaf66a94e114100dc7130a077bdc5326247
Trung Nam <60380159+namvo1999@users.noreply.github.com>
2026-04-05 20:07:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/Vd-hE4bIEU8AAAAi/amicat1-math.gif",  // 1 confused
+    "https://media.tenor.com/ag4FIgkgekYAAAAi/ami-fat-cat-please.gif",             // 2 pleading
+    "https://media.tenor.com/IqcGhw6ByxMAAAAi/ami-fat-cat-sad.gif",       // 4 sadder
+    "https://media.tenor.com/IxZpDCuRsPcAAAAi/amy-cat-cute-cat.gif",             // 3 sad
+    "https://media.tenor.com/oWZqn6GmscUAAAAi/amicat1-waving.gif",             // 5 devastated
+    "https://media1.tenor.com/m/UHtDixeNbMAAAAAC/ami-fat-cat-fgcat.gif",               // 6 very devastated
+    "https://media1.tenor.com/m/nlCp-sK1oRcAAAAC/ami-fat-cat-fgcat.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
